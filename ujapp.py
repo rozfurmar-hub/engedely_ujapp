6da4c1c3765ad1851ee42df8667e2afbaa4116a1
rozfurmar-hub <rozfurmar@gmail.com>
2026-05-02 19:12:18 +0200

    Update ujapp.py

--- ujapp.py
+++ ujapp.py
@@ -680,7 +680,7 @@ with st.form("adaturlap", clear_on_submit=False):
     # Szálláshelyen tartózkodás jogcíme
     szallas_jogcim_disp = st.selectbox(
         L["szallas_jogcim"],
-        options=[""] + SZALLAS_JOGCIM_DISP,
+        options=[""] + (SZALLAS_JOGCIM_RU if ui_lang == "ru" else SZALLAS_JOGCIM),
         index=0,
         key="szallas_jogcim_disp"
     )
