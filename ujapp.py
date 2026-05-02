66a573c1191d36bb8fcb4e3ffe0c0b33f51ffbe5
rozfurmar-hub <rozfurmar@gmail.com>
2026-05-02 19:08:48 +0200

    Update ujapp.py

--- ujapp.py
+++ ujapp.py
@@ -678,12 +678,18 @@ with st.form("adaturlap", clear_on_submit=False):
     emelet = st.text_input(L["emelet"]) 
     ajto = st.text_input(L["ajto"]) 
     # Szálláshelyen tartózkodás jogcíme
-    szallas_jogcim_disp = st.selectbox(L["szallas_jogcim"], options=[""] + SZALLAS_JOGCIM_DISP, index=0, key="szallas_jogcim_disp")
+    szallas_jogcim_disp = st.selectbox(
+        L["szallas_jogcim"],
+        options=[""] + SZALLAS_JOGCIM_DISP,
+        index=0,
+        key="szallas_jogcim_disp"
+    )
+
     
     # "egyéb" esetén plusz szövegmező
     szallas_egyeb = ""
     if ui_lang == "ru":
-        egyeb_ertek = "другое"
+        egyeb_ertek = "прочее"
     else:
         egyeb_ertek = "egyéb"
     
